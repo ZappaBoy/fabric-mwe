387f5ba7aabcccf4af1e262b8b7cf77bc7d2b8ec
Zappone Federico <45740621+ZappaBoy@users.noreply.github.com>
2020-08-17 15:46:22 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -31,7 +31,7 @@ echo 1000 > identity-rca/crlnumber
 # Fix openssl config
 OPENSSL_CONFIG=$(cat /etc/ssl/openssl.cnf | sed "s/RANDFILE\s*=\s*\$ENV::HOME\/\.rnd/#/")
 # Generate the Identity Root CA's
-openssl ecparam -name prime256v1 -genkey -noout -out identity-rca/private/rca.identity.org1.example.com.key -config ${OPENSSL_CONFIG}
+openssl ecparam -name prime256v1 -genkey -noout -out identity-rca/private/rca.identity.org1.example.com.key
 
 # Based on the private key, generate a Certificate Signing Request (CSR) and self-sign the CSR
 # In case the following command generate an error add this (https://github.com/openssl/openssl/issues/7754#issuecomment-601176195): 
@@ -45,12 +45,12 @@ echo 1000 > tls-rca/serial
 echo 1000 > tls-rca/crlnumber
 
 # Based on the private key, generate a Certificate Signing Request (CSR) and self-sign the CSR
-openssl ecparam -name prime256v1 -genkey -noout -out tls-rca/private/rca.tls.org1.example.com.key -config ${OPENSSL_CONFIG}
+openssl ecparam -name prime256v1 -genkey -noout -out tls-rca/private/rca.tls.org1.example.com.key
 openssl req -config openssl_root-tls.cnf -new -x509 -sha256 -extensions v3_ca -key tls-rca/private/rca.tls.org1.example.com.key -out tls-rca/certs/rca.tls.org1.example.com.cert -days 3650 -subj "/C=IT/ST=Italy/L=Italy/O=org1.example.com/OU=Example/CN=rca.tls.org1.example.com" -config ${OPENSSL_CONFIG}
 
 # (Intermediate CA) Create Intermediate Certificate Authorities
 # Generate private key and CSR for Identity Intermediate CA. Note that the value of Organization (O) i.e. org1.example.com is the same as that of the Identity Root CA
-openssl ecparam -name prime256v1 -genkey -noout -out $ORG_DIR/ca/ica.identity.org1.example.com.key -config ${OPENSSL_CONFIG}
+openssl ecparam -name prime256v1 -genkey -noout -out $ORG_DIR/ca/ica.identity.org1.example.com.key
 # In case the following command generate an error add this (https://github.com/openssl/openssl/issues/7754#issuecomment-601176195): 
 # -config <(cat /etc/ssl/openssl.cnf | sed "s/RANDFILE\s*=\s*\$ENV::HOME\/\.rnd/#/")
 openssl req -new -sha256 -key $ORG_DIR/ca/ica.identity.org1.example.com.key -out $ORG_DIR/ca/ica.identity.org1.example.com.csr -subj "/C=IT/ST=Italy/L=Italy/O=org1.example.com/OU=Example/CN=ica.identity.org1.example.com" -config ${OPENSSL_CONFIG}
